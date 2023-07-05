979eb472f33c9189200b35d4e9dd36accbea0fae
KeniLazarus <milojkovic.stanko@gmail.com>
2023-07-05 13:27:09 +0200

    Adding files

--- step-definitions/steps.js
+++ step-definitions/steps.js
@@ -1,17 +1,14 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
 
 import LoginPage from '../support/pages/login.page.js';
+import MainPage from '../support/pages/main.page.js';
 
 Given(/^I am on the login page$/, async () => {
-    await browser.url("/")
-});
+    await browser.url("/")});
 
 When(/^I login with credentials$/, async () => {
-    await LoginPage.login('standard_user','secret_sauce')
-});
+    await LoginPage.login('standard_user','secret_sauce')});
 
 Then(/^I am logged in$/, async () => {
-    
-});
-
-
+    const $button = await $('//div[@id="shopping_cart_container"]')
+    await expect($button).toBeDisplayed()});
---
+++ support/pages/main.page.js
@@ -0,0 +1,6 @@
+class MainPage {
+
+    get ShoppingCartButton () {return $('//div[@id="shopping_cart_container"]')};}
+//TODO: komentar 1 2 3
+
+export default new MainPage();
